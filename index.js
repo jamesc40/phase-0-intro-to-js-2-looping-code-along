744b304786d106c1a828f728b3448857785933f2
James Coffman <jamesc4023@gmail.com>
2022-04-05 18:59:54 -0400

    commit tree 186266b26d1d8885334e569e43cf8a03ab4e103c

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    const tyCards = [];
+    for(let i = 0; i < names.length; i++){
+        tyCards[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    }
+    return tyCards;
+}
+
+function countDown(x){
+    while(x >= 0){
+        console.log(x);
+        x--;
+    }
+}
